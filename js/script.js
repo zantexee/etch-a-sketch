6be60df05604272400c448dff8aa0e8618bf7bb5
Flowertin Madalin <60981000+zantexee@users.noreply.github.com>
2021-03-18 14:57:47 +0200

    Added dynamic grid fill

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
-const GRID_SIZE = 16 * 16;
-const CLASS_NAME = "grid-item";
-
+const className = "grid-item";
 const gridContainer = document.querySelector(".grid-container");
 
-for (let i = 0; i < GRID_SIZE; i++) {
-  let gridItem = document.createElement("div");
-  gridItem.className = CLASS_NAME;
-  gridContainer.appendChild(gridItem);
-}
+//Generates random RGBa string.
+const randomRgbaString = () => {
+  let r = Math.floor(Math.random() * 255);
+  let g = Math.floor(Math.random() * 255);
+  let b = Math.floor(Math.random() * 255);
+  let a = Math.random() * 1;
+
+  return `rgba(${r}, ${g}, ${b}, ${a})`;
+};
+
+//Changes color of hovered item's background
+const changeItemBg = (item) =>
+  (item.style.backgroundColor = randomRgbaString());
+
+//Fill grid container.
+const fillGridContainer = (squaresPerSide) => {
+  let gridSize = squaresPerSide ? squaresPerSide : 16;
+  gridContainer.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr); grid-template-rows: repeat(${gridSize}, 1fr);`;
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    let gridItem = document.createElement("div");
+
+    gridItem.onmouseover = () => changeItemBg(gridItem);
+
+    gridItem.className = className;
+    gridContainer.appendChild(gridItem);
+  }
+};
+
+fillGridContainer(4);
